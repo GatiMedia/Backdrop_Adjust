2511e92f95406ad1f08b08f12faa80a8a4162806
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-11-01 21:59:17 +0000

    Create to_menu.py

---
+++ to_menu.py
@@ -0,0 +1,42 @@
+def create_BD_Adj():
+    node_Xpos = []
+    node_Ypos = []
+    z_List = []
+    if nuke.selectedNodes():
+        for s in nuke.selectedNodes():
+            try:
+                node_Xpos.append(s['xpos'].value())
+                node_Ypos.append(s['ypos'].value())
+                z_list.append(s['z_order'].value())
+            except:
+                pass
+
+        x_Max = max(node_Xpos)
+        x_Min = min(node_Xpos)
+
+        x_Width = (float(x_Max) - float(x_Min))
+
+        y_Max = max(node_Ypos)
+        y_Min = min(node_Ypos)
+
+        y_Height = (float(y_Max) - float(y_Min))
+
+        if not z_List:
+            z_List.append(0)
+        z_Min = min(z_List)
+
+        bd_this = nuke.createNode('Backdrop_Adjust')
+
+        bd_this.setXpos(int(x_Min) - 100)
+        bd_this['bdwidth'].setValue(x_Width + 300)
+        bd_this.setYpos(int(y_Min) - 200)
+        bd_this['bdheight'].setValue(y_Height + 300)
+        bd_this['z_order'].setValue(z_Min - 1)
+        bd_this.hideControlPanel()
+    else:
+        bd_that = nuke.createNode('Backdrop_Adjust')
+        bd_that['z_order'].setValue(0)
+        bd_that.hideControlPanel()
+
+
+nuke.menu('Nodes').addMenu('Other').addCommand('BackdropAdjust.', 'create_BD_Adj()', shortcut='ctrl+b', icon='Backdrop.png', index=3)

4fc2e544ff04396f2917aaf73cf131068c8da0a3
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-11-15 11:42:28 +0000

    Update to_menu.py

--- to_menu.py
+++ to_menu.py
@@ -25,9 +25,31 @@ def create_BD_Adj():
             z_List.append(0)
         z_Min = min(z_List)
 
-        #TODO add darker tile color part
+        bd_this = nuke.nodes.Backdrop_Adjust()
 
-        bd_this = nuke.createNode('Backdrop_Adjust')
+        ok_colors = [3149642751, 2863311615, 2576980479, 2290649343, 2004318207, 1717987071, 1431655935, 1145324799,
+                     572662527, 286331391, 255]
+
+        if nuke.selectedNodes('BackdropNode'):
+            existing_indexes = [0]
+            for bd in nuke.selectedNodes('BackdropNode'):
+                color = int(bd['tile_color'].getValue())
+                try:
+                    curr_index = ok_colors.index(color)
+                    existing_indexes.append(curr_index)
+                except ValueError:
+                    continue
+
+            new_index = sorted(existing_indexes)[-1] + 1
+
+            try:
+                bd_this['tile_color'].setValue(ok_colors[new_index])
+            except IndexError:
+                bd_this['tile_color'].setValue(ok_colors[-1])
+        else:
+            bd_this['tile_color'].setValue(3149642751)
+
+        #TODO fix size issue
 
         bd_this.setXpos(int(x_Min) - 100)
         bd_this['bdwidth'].setValue(x_Width + 300)
@@ -37,7 +59,7 @@ def create_BD_Adj():
         bd_this.hideControlPanel()
     else:
         bd_that = nuke.createNode('Backdrop_Adjust')
-        bd_that['tile_color'].setValue(1717987071)
+        bd_that['tile_color'].setValue(3149642751)
         bd_that['z_order'].setValue(0)
         bd_that.hideControlPanel()
 

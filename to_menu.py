ee00b9346feb1f75a6c7447a2bd729a7ef0e4a71
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-11-26 10:30:23 +0000

    Update to_menu.py

--- to_menu.py
+++ to_menu.py
@@ -1,79 +1,40 @@
 def create_BD_Adj():
-    node_xpos = []
-    node_ypos = []
     z_List = []
-    bd_xpos = []
-    bd_ypos = []
-    if nuke.selectedNodes('BackdropNode'):
-        sel_bd = nuke.selectedNodes('BackdropNode')
-        for s in sel_bd:
-            bd_xpos.append(s['xpos'].value())
-            bd_xpos.append(s['xpos'].value() + s['bdwidth'].value())
-            bd_ypos.append(s['ypos'].value())
-            bd_ypos.append(s['ypos'].value() + s['bdheight'].value())
-            z_List.append(s['z_order'].value())
-        else:
-            pass
-
     if nuke.selectedNodes():
-        for s in nuke.selectedNodes():
-            node_xpos.append(s['xpos'].value())
-            node_ypos.append(s['ypos'].value())
 
-        x_max = max(node_xpos)
-        x_min = min(node_xpos)
-
-        y_max = max(node_ypos)
-        y_min = min(node_ypos)
-
-        # Calculating the node's size in if the lowest right corner isn't a Backdrop but a node
-        if not bd_xpos:
-            bd_xpos.append(x_min)
-            bd_ypos.append(y_max)
-
-        if bd_xpos:
-            max_bd_xpos = max(bd_xpos)
-            min_bd_xpos = min(bd_xpos)
-
-            max_bd_ypos = max(bd_ypos)
-            min_bd_ypos = min(bd_ypos)
-
-            if max_bd_xpos > x_max:
-                x_extra = 0
+        if nuke.selectedNodes('BackdropNode'):
+            sel_bd = nuke.selectedNodes('BackdropNode')
+            for s in sel_bd:
+                z_List.append(s['z_order'].value())
             else:
-                x_extra = 80
-
-            if max_bd_ypos > y_max:
-                y_extra = 0
-            else:
-                y_extra = 30
-
-        else:
-            pass
-
-        x_max = max(node_xpos + bd_xpos)
-        x_min = min(node_xpos + bd_xpos)
-
-        x_width = (float(x_max) - float(x_min))
-
-        y_max = max(node_ypos + bd_ypos)
-        y_min = min(node_ypos + bd_ypos)
-
-        y_height = (float(y_max) - float(y_min))
-
+                pass
 
         if not z_List:
             z_List.append(0)
         z_Min = min(z_List)
 
 
+        nodes = nuke.selectedNodes()
+
+        # Calculate bounds for the backdrop node.
+        bdX = min([node.xpos() for node in nodes])
+        bdY = min([node.ypos() for node in nodes])
+        bdW = max([node.xpos() + node.screenWidth() for node in nodes]) - bdX
+        bdH = max([node.ypos() + node.screenHeight() for node in nodes]) - bdY
+
+        # Expand the bounds to leave a little border. Elements are offsets for left, top, right and bottom edges respectively
+        left, top, right, bottom = (-100, -200, 100, 100)
+        bdX += left
+        bdY += top
+        bdW += (right - left)
+        bdH += (bottom - top)
+
         # Creating the node
         bd_this = nuke.nodes.Backdrop_Adjust()
-
-        bd_this.setXpos(int(x_min) - 100)
-        bd_this['bdwidth'].setValue(int(x_width) + int(x_extra) + 200)
-        bd_this.setYpos(int(y_min) - 200)
-        bd_this['bdheight'].setValue(int(y_height) + int(y_extra) + 300)
+        bd_this["xpos"].setValue(bdX)
+        bd_this["bdwidth"].setValue(bdW)
+        bd_this["ypos"].setValue(bdY)
+        bd_this["bdheight"].setValue(bdH)
         bd_this['z_order'].setValue(z_Min - 1)
 
         # Handle tile_color by Borsari Nicola

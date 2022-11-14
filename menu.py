2bcce9dd68f8b861aadf260fda16faa60de66e04
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2022-11-14 17:56:23 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 #encoding=utf-8
 # --------------------------------------------------------------
 #  BackdropAdjust menu.py
-#  Version: 1.6
-#  Last Updated: 10/11/2022
+#  Version: 1.7
+#  Last Updated: 14/11/2022
 # --------------------------------------------------------------
 
 ## Imports
@@ -13,4 +13,4 @@ import Backdrop_Adjust_utils
 nuke.menu('Nodes').addMenu('Other').addCommand('BackdropAdjust', 'Backdrop_Adjust_utils.create_BD_Adj()', shortcut='Alt+b', icon='Backdrop.png', index=3)
 
 ## Add BackdropAdjust(popup) the Toolset
-nuke.menu('Nodes').addMenu('Other').addCommand('BackdropAdjust (popup)', 'Backdrop_Adjust_utils.createBDApopup()', shortcut='Alt+Shift+b', icon='Backdrop.png', index=4)
+nuke.menu('Nodes').addMenu('Other').addCommand('BackdropAdjust(popup)', 'Backdrop_Adjust_utils.createBDApopup()', shortcut='Alt+Shift+b', icon='Backdrop.png', index=4)
